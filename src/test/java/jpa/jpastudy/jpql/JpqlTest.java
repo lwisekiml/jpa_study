5046749f3a02e18445cdeb9da60a259c49a69962
wisekim <lwisekiml@naver.com>
2024-06-04 10:08:23 +0900

    조건식(case 등등)

--- src/test/java/jpa/jpastudy/jpql/JpqlTest.java
+++ src/test/java/jpa/jpastudy/jpql/JpqlTest.java
@@ -224,4 +224,66 @@ public class JpqlTest {
         }
 
     }
+
+    @Test
+    public void 조건식_case등() {
+        Team team = new Team();
+        team.setName("teamA");
+        em.persist(team);
+
+        Member member = new Member();
+        member.setUsername("teamA");
+        member.setAge(10);
+        member.setType(MemberType.ADMIN);
+
+        member.setTeam(team);
+
+        em.persist(member);
+
+        em.flush();
+        em.clear();
+
+        String query =
+                "select " +
+                        "case when m.age <= 10 then '학생요금'" +
+                        "     when m.age >= 60 then '경로요금'" +
+                        "     else '일반요금' " +
+                        " end " +
+                        "from Member m";
+        List<String> result = em.createQuery(query, String.class)
+                .getResultList();
+
+        for (String s : result) {
+            System.out.println("s = " + s); // 학생요금
+        }
+    }
+
+    @Test
+    public void 조건식_coalesce() {
+        Team team = new Team();
+        team.setName("teamA");
+        em.persist(team);
+
+        Member member = new Member();
+        member.setUsername(null); // nullif 할 때 member로
+        member.setAge(10);
+        member.setType(MemberType.ADMIN);
+
+        member.setTeam(team);
+
+        em.persist(member);
+
+        em.flush();
+        em.clear();
+        // coalesce : 하나씩 조회해서 null이 아니면 반환
+        // NULLIF : 두 값이 같으면 null 반환, 다르면 첫번째 값 반환
+        // coalesce(m.username, '이름 없는 회원') as username 이런식으로도 사용 가능
+        String query = "select coalesce(m.username, '이름 없는 회원') from Member m";
+//        String query = "select nullif(m.username, 'member') from Member m";
+        List<String> result = em.createQuery(query, String.class).getResultList();
+
+        for (String s : result) {
+            System.out.println("s = " + s); // 이름 없는 회원
+        }
+    }
 }

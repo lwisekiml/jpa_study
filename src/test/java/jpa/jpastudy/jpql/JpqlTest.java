17c38b7ba530d7d216b84f6426a9efb4794f048f
wisekim <lwisekiml@naver.com>
2024-06-04 12:58:25 +0900

    경로 표현식

--- src/test/java/jpa/jpastudy/jpql/JpqlTest.java
+++ src/test/java/jpa/jpastudy/jpql/JpqlTest.java
@@ -348,4 +348,52 @@ public class JpqlTest {
             System.out.println("s = " + s); // s = 관리자1,관리자2
         }
     }
+
+    @Test
+    public void 경로표현식() {
+        Team team = new Team();
+        team.setName("teamA");
+        team.setAge(20);
+        em.persist(team);
+
+        Member member1 = new Member();
+        member1.setUsername("관리자1");
+        member1.setTeam(team);
+        em.persist(member1);
+
+        Member member2 = new Member();
+        member2.setUsername("관리자2");
+        member2.setTeam(team);
+        em.persist(member2);
+
+        em.flush();
+        em.clear();
+
+        // 1. 상태 필드 : 경로 탐색의 끝, 탐색X / m.username
+//            String query = "select m.username from Member m";
+
+        // 2. 단일 값 연관 경로 : 묵시적 내부 조인, 탐색 O -> 내부 조인이 발생하게 짜면 안된다.(튜닝의 어려움)/ m.team
+//            String query = "select m.team from Member m";
+//            List<Team> result = em.createQuery(query, Team.class)
+//                    .getResultList();
+//
+//            for (Team s : result) {
+//                System.out.println("s = " + s);
+//            }
+
+        System.out.println("===================================================================================");
+//        String q = "select t.members.size from Team t";
+//        Integer r = em.createQuery(q, Integer.class).getSingleResult();
+//        System.out.println("r = " + r); // 2
+        System.out.println("===================================================================================");
+
+        // 3. 컬렉션 값 연관 경로 : 묵시적 내부 조인 발생, 탐색 X
+        // t.members는 컬렉션이기 때문에 .으로 다른 값을 사용 불가능 하다.
+//        String query = "select t.members from Team t";
+        // 그래서 아래와 같이 사용해야 한다.
+        String query = "select m.username from Team t join t.members m";
+        List<String> result = em.createQuery(query, String.class).getResultList();
+
+        System.out.println("result = " + result);
+    }
 }
